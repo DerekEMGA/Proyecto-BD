41c9ac307857f46002179844bb8085db5d85c470
Derek Gonzalez <115957604+DerekEMGA@users.noreply.github.com>
2022-11-19 13:31:13 -0600

    Elimine el hideSubMenu()

--- Consultorio Medico ITS/Principal.vb
+++ Consultorio Medico ITS/Principal.vb
@@ -4,12 +4,7 @@
 
     End Sub
 
-    Public Sub hideSubmenu()
-        Panel_Carreras.Visible = False
-        Panel_Consultas.Visible = False
-        Panel_Alumnos.Visible = False
-        Panel_Departamento.Visible = False
-        Panel_Medicos.Visible = False
+  
 
     End Sub
 
